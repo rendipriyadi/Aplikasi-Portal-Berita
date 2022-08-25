130cee951a29e104806d70e74538317b8a1ad244
Rendi Priyadi <rendipriyadi0410@gmail.com>
2022-08-25 09:40:05 +0700

    first commit

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\News;
+use App\Http\Resources\NewsCollection;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $news = new NewsCollection(News::OrderByDesc('id')->paginate(8));
+        return Inertia::render('Homepage', [
+            'title' => "CUY UNIVERSE HOME",
+            'description' => "Selamat Datang Di Cuy Universe News Portal",
+            'news' => $news,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $news = new News;
+
+        $news->title = $request->title;
+        $news->description = $request->description;
+        $news->category = $request->category;
+        $news->author = auth()->user()->email;
+
+        $news->save();
+        return redirect()->back()->with('message', 'Data Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function show(news $news)
+    {
+        $myNews = $news::where('author', auth()->user()->email)->OrderByDesc('id')->get();
+        return Inertia::render('Dashboard', [
+            'myNews' => $myNews,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(news $news, Request $request)
+    {
+        return Inertia::render('EditNews', [
+            'myNews' => $news->find($request->id)
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        News::where('id', $request->id)->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'category' => $request->category,
+        ]);
+
+        return to_route('dashboard')->with('message', 'Update Data Berhasil');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\news  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $news = News::find($request->id);
+        $news->delete();
+        return redirect()->back()->with('message', 'Data Berhasil Dihapus');
+    }
+}
